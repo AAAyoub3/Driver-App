58e1d6e94cd2f107f19526f0ca31a3fd89af40dc
YussefCodez <youseffortech@gmail.com>
2026-05-05 15:20:22 +0300

    + init core

---
+++ lib/core/l10n/translations/app_localizations_ar.dart
@@ -0,0 +1,163 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get name_is_required => 'الاسم مطلوب!';
+
+  @override
+  String get name_is_not_valid => 'الاسم غير صحيح';
+
+  @override
+  String get email_is_required => 'البريد الإلكتروني مطلوب!';
+
+  @override
+  String get email_is_not_valid => 'البريد الإلكتروني غير صحيح';
+
+  @override
+  String get password_is_required => 'كلمة المرور مطلوبة!';
+
+  @override
+  String get password_is_not_valid => 'كلمة المرور غير صحيحة';
+
+  @override
+  String get password_must_be_at_least_6_characters =>
+      'كلمة المرور يجب أن تكون 6 أحرف على الأقل';
+
+  @override
+  String get passwords_do_not_match => 'كلمتا المرور غير متطابقتين';
+
+  @override
+  String get confirm_password_is_required => 'تأكيد كلمة المرور مطلوب!';
+
+  @override
+  String get confirm_password_is_not_valid => 'تأكيد كلمة المرور غير صحيح';
+
+  @override
+  String get password_and_confirm_password_must_be_same =>
+      'يجب أن تكون كلمتا المرور متطابقتين';
+
+  @override
+  String get phone_number_is_required => 'رقم الهاتف مطلوب!';
+
+  @override
+  String get phone_number_is_not_valid => 'رقم الهاتف غير صحيح';
+
+  @override
+  String get this_field_is_required => 'هذا الحقل مطلوب';
+
+  @override
+  String get error => 'خطأ';
+
+  @override
+  String get ok => 'حسناً';
+
+  @override
+  String get hey_there => 'مرحباً';
+
+  @override
+  String get welcome_back => 'مرحباً بعودتك';
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forget_password_ques => 'نسيت كلمة المرور؟';
+
+  @override
+  String get register => 'إنشاء حساب';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get settings => 'الإعدادات';
+
+  @override
+  String get help => 'مساعدة';
+
+  @override
+  String get dark_mode => 'الوضع الداكن';
+
+  @override
+  String get light_mode => 'الوضع الفاتح';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get profile => 'الملف الشخصي';
+
+  @override
+  String get hi => 'مرحباً';
+
+  @override
+  String get apply => 'تقديم';
+
+  @override
+  String get what_is_new => 'ما الجديد؟';
+
+  @override
+  String get sea_all => 'عرض الكل';
+
+  @override
+  String get enter_your_username_and_password_to_login =>
+      'أدخل اسم المستخدم وكلمة المرور لتسجيل الدخول';
+
+  @override
+  String get username => 'اسم المستخدم';
+
+  @override
+  String get or_login_with => 'أو سجل باستخدام';
+
+  @override
+  String get don_t_have_an_account => 'ليس لديك حساب؟';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get confirm_password => 'تأكيد كلمة المرور';
+
+  @override
+  String get mobile_number => 'رقم الهاتف';
+
+  @override
+  String get register_as => 'التسجيل كـ';
+
+  @override
+  String get google => 'جوجل';
+
+  @override
+  String get facebook => 'فيسبوك';
+
+  @override
+  String get select_an_option => 'اختر خياراً';
+
+  @override
+  String get login_success => 'تم تسجيل الدخول بنجاح';
+
+  @override
+  String get register_success => 'تم التسجيل بنجاح';
+
+  @override
+  String get connectButton => 'ربط';
+
+  @override
+  String get an_error_occurred => 'حدث خطأ';
+}
