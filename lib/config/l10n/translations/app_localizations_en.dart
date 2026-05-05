fcfe4002fb1c3947840d679d59c6450d8a320971
Abdelrahman Mostafa <abdomostafamohamed1234@gmail.com>
2026-05-05 15:03:56 +0300

    feat: add packages and localizations and routs

---
+++ lib/config/l10n/translations/app_localizations_en.dart
@@ -0,0 +1,164 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get name_is_required => 'Name is required!';
+
+  @override
+  String get name_is_not_valid => 'This name is not valid';
+
+  @override
+  String get email_is_required => 'Email is required!';
+
+  @override
+  String get email_is_not_valid => 'This email is not valid';
+
+  @override
+  String get password_is_required => 'Password is required!';
+
+  @override
+  String get password_is_not_valid => 'This password is not valid';
+
+  @override
+  String get password_must_be_at_least_6_characters =>
+      'Password must be at least 6 characters';
+
+  @override
+  String get passwords_do_not_match => 'Passwords do not match';
+
+  @override
+  String get confirm_password_is_required => 'Confirm password is required!';
+
+  @override
+  String get confirm_password_is_not_valid =>
+      'This confirm password is not valid';
+
+  @override
+  String get password_and_confirm_password_must_be_same =>
+      'Password and confirm password must be same!';
+
+  @override
+  String get phone_number_is_required => 'Phone number is required!';
+
+  @override
+  String get phone_number_is_not_valid => 'This phone number is not valid';
+
+  @override
+  String get this_field_is_required => 'This field is required';
+
+  @override
+  String get error => 'Error';
+
+  @override
+  String get ok => 'Ok';
+
+  @override
+  String get hey_there => 'Hey There';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forget_password_ques => 'Forget Password ?';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get or => 'Or';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get help => 'Help';
+
+  @override
+  String get dark_mode => 'Dark mode';
+
+  @override
+  String get light_mode => 'Light mode';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get hi => 'Hi';
+
+  @override
+  String get welcome_back => 'Welcome back';
+
+  @override
+  String get apply => 'Apply';
+
+  @override
+  String get what_is_new => 'What’s New?';
+
+  @override
+  String get sea_all => 'Sea All';
+
+  @override
+  String get enter_your_username_and_password_to_login =>
+      'Enter your username and password to login';
+
+  @override
+  String get username => 'Username';
+
+  @override
+  String get or_login_with => 'Or login with';
+
+  @override
+  String get don_t_have_an_account => 'Don\'t have an account?';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get confirm_password => 'Confirm Password';
+
+  @override
+  String get mobile_number => 'Mobile Number';
+
+  @override
+  String get register_as => 'Register As';
+
+  @override
+  String get google => 'Google';
+
+  @override
+  String get facebook => 'Facebook';
+
+  @override
+  String get login_success => 'LoginSuccessfully';
+
+  @override
+  String get register_success => 'Successfully';
+
+  @override
+  String get select_an_option => 'Select an option';
+
+  @override
+  String get connectButton => 'Connect';
+
+  @override
+  String get an_error_occurred => 'An error occurred';
+}
---
+++ lib/config/routing/routing_generator.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+import 'app_routes.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      // case AppRoutes.login:
+      //   return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: const Text('No Route Found')),
+        body: const Center(child: Text('No Route Found')),
+      ),
+    );
+  }
+}
