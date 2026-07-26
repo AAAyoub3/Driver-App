cd699916d263e19f52f7251e9270a830906baa2e
Noor Elden Wael <154272325+NoorEldenWael@users.noreply.github.com>
2026-07-26 07:36:34 +0300

    ..

---
+++ lib/modules/profile/data/models/responses/edit_vieckle_response.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_vieckle_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EditVieckleResponse _$EditVieckleResponseFromJson(Map<String, dynamic> json) =>
+    EditVieckleResponse(
+      message: json['message'] as String?,
+      error: json['error'] as String?,
+    );
+
+Map<String, dynamic> _$EditVieckleResponseToJson(
+  EditVieckleResponse instance,
+) => <String, dynamic>{'message': instance.message, 'error': instance.error};
